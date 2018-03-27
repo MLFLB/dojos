e30676d5eaf3c64fb931c9a16034d9a162141617
AlexLoWCD <alexandre.lorion@wildcodeschool.fr>
2018-03-27 17:46:48 +0200

    Ajout solution

--- dojo_orderwords/solution.js
+++ dojo_orderwords/solution.js
@@ -9,60 +9,77 @@ Ex:
 
 */
 
+
+// VERSION LONGUE
 const orderwords = (_arrayWords) => {
   // Je créé une variable "resultArray", initialisée à un tableau vide.
   // Ce tableau contiendra mes différents mots mis dans l'ordre d'apparition.
+  let resultArray = [];
 
   // Je parcours mon tableau de mots
-  for () {
+  for (let i = 0; i < _arrayWords.length; i++) {
 
     // Je créé une variable "word" qui contiendra l'élement de
     // mon tableau "_arrayWords" à l'index "i".
+    let word = _arrayWords[i];
 
     // Je créé une variable "splittedWord" à laquelle on affectera un tableau
     //  correspondant à mon mot découpé par caractère.
 
+    let splittedWord = word.split("");
+
     // Je créé une variable "position" qui contiendra l'index du mot en cours.
     // Par défaut on affectera la valeur 0.
+    let position = 0;
 
     // Je vais créer une variable "indexWordIndex" qui va me permettre de
     // stocker l'emplacement de l'index de "word".
     // Par défaut on affectera la valeur 0.
 
+    let indexWordIndex = 0;
+
     // Je parcours mon tableau de caractère à la recherche de l'index
-    for () {
+    for (let j = 0; j < splittedWord.length; j++) {
 
       // Je créé une variable "letter" qui contiendra l'élement de
       // mon tableau "splittedWord" à l'index "j".
+      let letter = splittedWord[j];
 
       // Je créé une variable "letterToNumber" à laquelle on affectera
       // le résultat de la transformation de "letter" en nombre.
+      let letterToNumber = parseInt(letter);
 
       // Si mon caractère est transformable en nombre, alors je suis l'index.
       // Je sauvegarde l'emplacement de l'index.
+      if (!Number.isNaN(letterToNumber)) {
+        position = letterToNumber;
+        indexWordIndex = j;
+      }
     }
 
     // Une fois mon index récupéré, je vais devoir enlever cet index à "word".
     // Je vais créer une nouvelle variable appelée "wordWithoutIndex"
     // qui contiendra le mot sans l'index.Cette variable sera initialiée à
     // une chaîne de caractère vide.
+    let wordWithoutIndex = "";
 
     // Je découpe le mot afin de récupérer le mot sans l'index.
     //(String.prototype.slice)
+    wordWithoutIndex += word.slice(0, indexWordIndex);
+    wordWithoutIndex += word.slice(indexWordIndex + 1, word.length);
 
     // J'ajoute le mot reconstruit à l'index correspondant dans le tableau.
+    resultArray[position - 1] = wordWithoutIndex;
   }
 
   // Je créé une variable "result" qui va contenir la concaténation de
   // l'ensemble du tableau pour former une phrase.
   // (Array.prototype.join)
+  let result = resultArray.join(" ");
 
   // Je retourne le résultat
+  return result;
 }
 
-
-const ow1 = orderwords(["fai7re", "es3t", "1Ce", "c4arrément", "doj2o", "gén5ial", "à6"]);
-console.log(ow1 === "Ce dojo est carrément génial à faire");
-
-const ow2 = orderwords(["truc7s", "aujo8urd'hui", "6de", "forma2teur", "L1e", "beauco5up", "JS3", "m'a4pprend"]);
-console.log(ow2 === "Le formateur JS m'apprend beaucoup de trucs aujourd'hui");
+// VERSION COURTE
+const orderwordsSmall = (_arrayWords) => _arrayWords.sort((a, b) => a.match(/\d/g) - b.match(/\d/g)).map(a => a.replace(/\d/g, "")).join(" ");
