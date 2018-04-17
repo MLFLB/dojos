498db50bf56892d2b0df6d41f0d8130f6a8b1c7e
AlexLoWCD <alexandre.lorion@wildcodeschool.fr>
2018-04-17 11:03:02 +0200

    End Dojo try catch

--- dojo_trycatch/trycatch.js
+++ dojo_trycatch/trycatch.js
@@ -1,2 +1,12 @@
 const json1 = '{"name": "Bruce", "lastname": "Wayne", "city": "Gotham"}';
 const json2 = '{name: "Bruce", lastname: "Wayne", city: "Gotham"}';
+
+
+try {
+  console.log(JSON.parse(json1));
+  console.log(JSON.parse(json2));
+} catch (error) {
+  console.error('JSON non valide');
+  console.log("erreur: ", error);
+}
+console.log('termine');
